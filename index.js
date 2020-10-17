f70006adb43de6747d0733dd286a65d41bc4630e
Mishra! <shubham.dakshana15@gmail.com>
2020-10-17 14:23:14 +0530

    Benchmarking factor

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Limiting no of threapools
+// Total remains same; each child gets one
+process.env.UV_THREADPOOL_SIZE = 1;
 
 const cluster = require('cluster');
 
@@ -6,6 +9,7 @@ if (cluster.isMaster) {
   // cause *index.js* to be executed again but in child mode
   // Every fork creates one instance
   // One fork starts up one child; anytime server is started
+  // Every single child has their own thread pool
   cluster.fork();
   cluster.fork();
   cluster.fork();
@@ -15,18 +19,24 @@ if (cluster.isMaster) {
   // I'm a child
   // I'm going to act like a server and nothing else
   const express = require('express');
+  const crypto = require('crypto');
+
   const app = express();
 
-  function doWork(duration) {
-    const start = Date.now();
-    while(Date.now() - start < duration) {
-      // do some stuff
-    }
-  }
+  // Pauses for exact amount of time always;
+  // Using other function to simulate real world; i.e. takes diff time to complete when executed
+  // function doWork(duration) {
+  //   const start = Date.now();
+  //   while(Date.now() - start < duration) {
+  //     // do some stuff
+  //   }
+  // }
 
   app.get('/', (req, res, next) => {
-    doWork(5000);
-    res.send("Hey there!!");
+    // doWork(5000);
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      res.send("Hey there!!");
+    })
   })
 
   app.get('/fast', (req, res, next) => {
