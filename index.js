780cb827ea61db0850a68a4e57c50d34edc8a9cf
Mishra! <shubham.dakshana15@gmail.com>
2020-10-17 14:08:13 +0530

    Clustering in action

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 
-const express = require('express');
+const cluster = require('cluster');
 
-const app = express();
+// Is the file being executed in master mode
+if (cluster.isMaster) {
+  // cause *index.js* to be executed again but in child mode
+  // Every fork creates one instance
+  // One fork starts up one child; anytime server is started
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
 
-app.get('/', (req, res, next) => {
-  res.send("Hey there!!");
-})
+} else {
+  // I'm a child
+  // I'm going to act like a server and nothing else
+  const express = require('express');
+  const app = express();
 
-app.listen(3000);
+  function doWork(duration) {
+    const start = Date.now();
+    while(Date.now() - start < duration) {
+      // do some stuff
+    }
+  }
+
+  app.get('/', (req, res, next) => {
+    doWork(5000);
+    res.send("Hey there!!");
+  })
+
+  app.get('/fast', (req, res, next) => {
+    res.send("Hey there! This is faster.");
+  })
+
+  app.listen(3000);
+}
