ea687ddacdc6d8f3d5f95c978b4dcc430b0c95ed
Mishra! <shubham.dakshana15@gmail.com>
2020-10-19 10:00:42 +0530

    Cache implementation

--- services/cache.js
+++ services/cache.js
@@ -2,19 +2,33 @@
 // See mongoose source code
 const mongoose = require('mongoose');
 
+const redisClient = require('../config/redis');
+
 const exec = mongoose.Query.prototype.exec;
 
 // Hooking into mongoose query execution
 // It creates query and calls .exec on query
 // So Checking in redis if query is already called before executing query
-mongoose.Query.prototype.exec = function() {
+mongoose.Query.prototype.exec = async function() {
   console.log("Running a query...", this.getQuery());   // Query being executed
   console.log("Collection running query...", this.mongooseCollection.name)    // Name of the Collection that called the query
 
   // Creation of unique REDIS_KEY
-  const redisKey = Object.assign({}, this.query(), {
+  const redisKey = JSON.stringify(Object.assign({}, this.query(), {
     collection: this.mongooseCollection.name
-  });
+  }));
 
-  return exec.apply(this, arguments);
+  // Checking if "redisKey" is already present in REDIS
+  const cacheValue = await redisClient.get(redisKey);
+  if (cacheValue) {
+    // If present in REDIS, then return the cacheValue
+    console.log("Returning cached value...");
+    return JSON.parse(cacheValue);
+  }
+
+  // If cacheValue not present; execute query, store in redis and return queryResult
+  console.log("Fetching and Returning from db...");
+  const queryResult = await exec.apply(this, arguments);
+  redisClient.set(redisKey, JSON.stringify(result));
+  return queryResult;
 }
