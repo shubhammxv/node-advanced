42d6422554a775e8ef4159b3f54ba52dfcde6d17
Mishra! <shubham.dakshana15@gmail.com>
2020-10-19 10:26:54 +0530

    Toggling caching using a chained fn

--- services/cache.js
+++ services/cache.js
@@ -6,6 +6,17 @@ const redisClient = require('../config/redis');
 
 const exec = mongoose.Query.prototype.exec;
 
+// Instead of caching all queries; caching only queries having .cache() function chained
+// So adding function to Query prototype; makes accesible to all queries
+mongoose.Query.prototype.cache = function() {
+  // this points to Query instance; every instance has its own this
+  this._cacheQuery = true;
+
+  // Makes chainable to any other function at any point;
+  // As the instance is returned the same as from any other function
+  return this;
+}
+
 // Hooking into mongoose query execution
 // It creates query and calls .exec on query
 // So Checking in redis if query is already called before executing query
@@ -13,6 +24,9 @@ mongoose.Query.prototype.exec = async function() {
   console.log("Running a query...", this.getQuery());   // Query being executed
   console.log("Collection running query...", this.mongooseCollection.name)    // Name of the Collection that called the query
 
+  if (!this._cacheQuery) {
+    return exec.apply(this, arguments);
+  }
   // Creation of unique REDIS_KEY
   const redisKey = JSON.stringify(Object.assign({}, this.query(), {
     collection: this.mongooseCollection.name
