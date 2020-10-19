25baaffb4ba47bb2367f79e2c1fc08f01d04f9d0
Mishra! <shubham.dakshana15@gmail.com>
2020-10-19 09:45:26 +0530

    Unique cache key creation

--- services/cache.js
+++ services/cache.js
@@ -8,6 +8,13 @@ const exec = mongoose.Query.prototype.exec;
 // It creates query and calls .exec on query
 // So Checking in redis if query is already called before executing query
 mongoose.Query.prototype.exec = function() {
-  console.log("Running a query...");
+  console.log("Running a query...", this.getQuery());   // Query being executed
+  console.log("Collection running query...", this.mongooseCollection.name)    // Name of the Collection that called the query
+
+  // Creation of unique REDIS_KEY
+  const redisKey = Object.assign({}, this.query(), {
+    collection: this.mongooseCollection.name
+  });
+
   return exec.apply(this, arguments);
 }
