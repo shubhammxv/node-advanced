c2a7054faf189d5a8d1a6c3cadd184c60f392bcc
Mishra! <shubham.dakshana15@gmail.com>
2020-10-21 09:30:32 +0530

    Refactoring and browser termination

--- tests/header.test.js
+++ tests/header.test.js
@@ -2,24 +2,34 @@
 // All ops are async in puppeteer
 const puppeteer = require('puppeteer');   // To Launch a new chromium instance
 
-test('Adds two numbers', () => {
-  const sum = 1 + 2;
-  expect(sum).toEqual(3);     // assert, should
-})
+// test('Adds two numbers', () => {
+//   const sum = 1 + 2;
+//   expect(sum).toEqual(3);     // assert, should
+// })
 
-test('Can launch a browser', async () => {
+let browser, page;
+
+// Runs before every test
+beforeEach(async () => {
   // Making headless false we can see gui in screen
   // Normally run in headless mode; makes tests run faster
-  const browser = await puppeteer.launch({
+  browser = await puppeteer.launch({
     headless: false
   });
 
   //Creating a new tab in browser instance
-  const page = await browser.newPage();
-
+  page = await browser.newPage();
   // Navigate to localhost in tab
   await page.goto('localhost:3000');
+})
 
+// Runs after each test; like a cleanup for browser instances
+afterEach(async () => {
+  // Close the running browser instance
+  await browser.close();
+})
+
+test('Can launch a browser', async () => {
   // Extracting text from logo using css selector
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
   expect(text).toEqual('Blogster');
