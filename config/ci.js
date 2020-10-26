c61a8ba2de9c1f030fd310ddb69024aa9ad93119
Mishra! <shubham.dakshana15@gmail.com>
2020-10-26 13:04:29 +0530

    More server config

--- config/ci.js
+++ config/ci.js
@@ -4,7 +4,7 @@ const dbCredentials = `${process.env.mongoUser}:${process.env.mongoPassword}`;
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: `mongodb://${dbCredentials}@cluster0.bdbz0.mongodb.net/blogs?retryWrites=true&w=majority`,
+  mongoURI: `mongodb://127.0.0.1:27017/blogs_ci?retryWrites=true&w=majority`,
   cookieKey: '123123123',
   redisURL: 'redis://127.0.0.1:6379'
 };
