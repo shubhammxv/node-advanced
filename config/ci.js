2d7f72a8fd0e198a5c5d09d490077faffbff3a2f
Mishra! <shubham.dakshana15@gmail.com>
2020-10-27 10:10:34 +0530

    Adding image file upload to AWS S3

--- config/ci.js
+++ config/ci.js
@@ -6,5 +6,7 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: `mongodb://127.0.0.1:27017/blogs_ci?retryWrites=true&w=majority`,
   cookieKey: '123123123',
-  redisURL: 'redis://127.0.0.1:6379'
+  redisURL: 'redis://127.0.0.1:6379',
+  s3AccessKey: '',
+  s3SecretKey: ''
 };
--- config/dev.js
+++ config/dev.js
@@ -6,5 +6,7 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: `mongodb://${dbCredentials}@cluster0.bdbz0.mongodb.net/blogs?retryWrites=true&w=majority`,
   cookieKey: '123123123',
-  redisURL: 'redis://127.0.0.1:6379'
+  redisURL: 'redis://127.0.0.1:6379',
+  s3AccessKey: process.env.S3_ACCESS_KEY,
+  s3SecretKey: process.env.S3_SECRET_KEY
 };
